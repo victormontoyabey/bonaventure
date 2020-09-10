f238e94ee2bf371a0474eeb88ca6440a9e7c142f
Victor Montoya <victormontoyabey@gmail.com>
2020-09-09 17:58:47 -0700

    Created a responsive navbar using Bulma. Added my own font styling through my own CSS documents and used JavaScript to make the burger icon functional.

---
+++ script.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector(".navbar-burger");
+let navMenu = document.querySelector(".navbar-menu");
+
+burger.addEventListener("click", function () {
+  event.preventDefault();
+  burger.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+});
